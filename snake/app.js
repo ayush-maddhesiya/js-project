5297b47ad974b66e368f546b0e68a3fd60be3ae5
Ayush Maddhesiya <118817485+ayush-maddhesiya@users.noreply.github.com>
2024-02-04 13:27:31 +0530

    now game is funtional(snake game)

--- snake/app.js
+++ snake/app.js
@@ -1,11 +1,11 @@
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
-
-ctx.fillRect(0,0,5,5);
+//ctx.fill = "blue"
+// ctx.fillRect(0,0,5,5);
 
 let boardHeight = 250
 let boardWidth = 500
-let square = 5;
+let square = 20;
 
 let snakeCells = [[0,0]];
 
@@ -14,14 +14,15 @@ let direction  = 'right';
 let gameOver = false;
 
 let foodCell = generateRandomCell();
+console.log(foodCell);
 
 let score = 0;
 let increaseScore  = 5;
 
 function generateRandomCell(){
     return [
-        Math.floor(Math.random()*500),
-        Math.floor(Math.random()*250),
+        (Math.floor(Math.random()*(500/20))*20),
+        (Math.floor(Math.random()*(250/20))*20)
     ]
 }
 
@@ -42,18 +43,14 @@ document.addEventListener("keydown",function (event) {
         case "ArrowUp":
             direction = 'up'
                 break;
-               
-        default:
-            alert("Plz Press valid key!!")
-            break;
     }
 })
 
 
 let gameLoop = setInterval(function () {
     updateSnake();
-    // drawSnake();
-},600);
+    drawSnake();
+6},500);
 
 
 function updateSnake() {
@@ -63,61 +60,82 @@ function updateSnake() {
     let headY = snakeCells[snakeCells.length-1][1];
 
     //this is contain new cells for adding lenght of snake.
-    let newX;
-    let newY;
+    let newX,newY;
 
     //actully updating new postion of cells.
 
-    if(newX === boardWidth || newY === boardWidth || newX < 0  || newY < 0){
-        gameOver = true;
-        return;
-    }
-    else{
-        if(direction === 'right'){
-            newX = headX + square;
-            newY = headY;
+    if (direction === 'right') {
+        newX = headX + square;
+        newY = headY;
+    
+        if (newX === boardWidth) {
+          gameOver = true;
         }
-        else if(direction === 'left'){
-            newX = headX - square;
-            newY = headY;
+        
+      } else if (direction === 'left') {
+        newX = headX - square;
+        newY = headY;
+    
+        if (newX < 0) {
+          gameOver = true;
         }
-        else if(direction === 'up'){
-            newX = headX ;
-            newY = headY + square;
-        }else{
-            newX = headX ;
-            newY = headY - square;
+    
+      } else if (direction === 'up') {
+        newX = headX;
+        newY = headY - square;
+    
+        if (newY < 0) {
+          gameOver = true;
         }
-    }
+    
+      } else {
+        newX = headX;
+        newY = headY + square;
+    
+        if (newY === boardHeight) {
+          gameOver = true;
+        }
+      }
+    
     snakeCells.push([newX,newY]);
 
-    if(foodCell[0] = headX && foodCell[1]  === headY){
+    if(foodCell[0] === headX && foodCell[1]  === headY){
         foodCell = generateRandomCell();
-        score+=increaseScore;
+        score  +=  increaseScore;
     }
     else{
         snakeCells.shift();
     }
 }
 
-function draw(){
+function drawSnake(){
     if(gameOver === true){
         clearInterval(gameLoop);
         ctx.fillStyle = 'red';
         ctx.font = '40px sans-serif';
         ctx.fillText('Game over', 100, 250);
-    return;
+        return;
     }
 
-    ctx.clearRect(0,0,square,square);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw the snake
     for(let cell of snakeCells){
-        ctx.fillStyle = 'Red';
-        ctx.fillRect(foodCell[0],foodCell[1],square,square);
+        ctx.fillStyle = 'red'; // You can change the color if you want
+        ctx.fillRect(cell[0], cell[1], square, square);
+        
+        ctx.fillStyle = 'pink'; // You can change the color if you want
+        ctx.fillRect(cell[0], cell[1], square, square);
+        
 
     }
 
-    ctx.fillStyle = 'pink';
+    // Draw the food cell
+    ctx.fillStyle = 'yellow';
     ctx.fillRect(foodCell[0], foodCell[1], square, square);
+
+    // Display the score
     ctx.font = '20px sans-serif';
+    ctx.fillStyle = 'black'; // You can change the color if you want
     ctx.fillText(`Score: ${score}`, 50, 50);
 }
